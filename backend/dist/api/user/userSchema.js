b0d92aa5a09ce85845a8655fcd4aa1c00d337ec4
nattyemu <natnaelbayut@gmail.com>
2024-12-28 13:03:28 +0300

    modify zod of register

--- backend/dist/api/user/userSchema.js
+++ backend/dist/api/user/userSchema.js
@@ -3,20 +3,41 @@ const userSchema = {
   register: z
     .object({
       email: z.string().email(),
-      password: z.string().min(8).max(20),
+      password: z.string().min(8).max(50),
       role: z.string(),
-      studentId: z.string().optional(),
-      faculty: z.string().optional(),
-      department: z.string().optional(),
+      studentId: z.string().nullable().optional(),
+      faculty: z.string().nullable().optional(),
+      department: z.string().nullable().optional(),
+      academicYear: z.string().nullable().optional(),
       firstName: z.string(),
       middleName: z.string(),
       lastName: z.string(),
       sex: z.enum(["MALE", "FEMALE"]),
-      academicYear: z.string().optional(),
+
       imageUrl: z.string().optional(),
       activeStatus: z.boolean().optional(),
     })
+    .refine(
+      (data) => {
+        if (data.role === "STUDENT") {
+          // Ensure that the required fields are present when the role is "STUDENT"
+          return (
+            data.faculty &&
+            data.department &&
+            data.studentId &&
+            data.academicYear
+          );
+        }
+        return true; // Return true for non-STUDENT roles (no validation required)
+      },
+      {
+        message:
+          "Faculty, department, studentId, and academicYear are required for students",
+        path: ["faculty"], // You can choose which field to flag the error on, e.g., 'faculty'
+      }
+    )
     .superRefine((data, ctx) => {
+      // Additional validation for 'academicYear' if the role is 'STUDENT'
       if (data.role === "STUDENT" && !data.academicYear) {
         ctx.addIssue({
           path: ["academicYear"],
@@ -25,6 +46,7 @@ const userSchema = {
         });
       }
     }),
+
   updateAdminUserSchema: z.object({
     email: z.string().email(),
     password: z.string().min(0).max(50).optional(),
@@ -45,14 +67,14 @@ const userSchema = {
     email: z.string().email().optional(),
 
     // Password field with conditional validation
-    password: z.string().min(0).max(20).optional(),
+    password: z.string().min(0).max(50).optional(),
     role: z.string().optional(),
   }),
   //define mother
   registerStudent: z.object({
     email: z.string().email(),
     phone: z.string().max(14),
-    password: z.string().min(8).max(20),
+    password: z.string().min(8).max(50),
     //profile
     firstName: z.string(),
     middleName: z.string(),
